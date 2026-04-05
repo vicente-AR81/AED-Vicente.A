5e6c5c2623025c81985234aeb988b2e5499b40b0
Vicente-AR81 <vicentearhex@gmail.com>
2026-04-05 17:43:29 -0300

    2 ejercicios mas

---
+++ AED/Ficha03 - Ejercicios/Ficha03-Ejercicio15.py
@@ -0,0 +1,10 @@
+#Centenario y bicentenario fecha patria
+
+Fecha = 2, 4, 1982
+
+Centenario = Fecha[0], Fecha[1], Fecha[2] + 100
+Bicentenario = Fecha[0], Fecha[1], Fecha[2] + 200
+
+print("Fecha Patria:", Fecha)
+print("Centenario:", Centenario)
+print("Bicentenario:", Bicentenario)
---
+++ AED/Ficha03 - Ejercicios/Ficha03-Ejercicio16.py
@@ -0,0 +1,11 @@
+#Tuplas y argentina campeona
+
+Mundial_1 = "Argentina", 1978
+Mundial_2 = "Mexico", 1986
+Mundial_3 = "Catar", 2022
+
+Dist_1 = Mundial_2[1] - Mundial_1[1]
+Dist_2 = Mundial_3[1] - Mundial_2[1]
+
+print("Los años desde el Argentina 78 hasta Mexico 86 fue de:", Dist_1, "Años")
+print("Los años desde desde Mexico 86 hasta catar fue de:", Dist_2, "Años")
